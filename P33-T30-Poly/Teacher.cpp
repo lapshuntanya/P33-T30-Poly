54a6797233bb0c8153974fdb0733fe994cabafb1
lapshuntanya <lapshun.tatiana@gmail.com>
2024-10-16 17:43:02 +0300

    defined Teacher

--- P33-T30-Poly/Teacher.cpp
+++ P33-T30-Poly/Teacher.cpp
@@ -1,36 +1,28 @@
 #include "Teacher.h"
 
-Teacher::Teacher()
-{
+Teacher::Teacher(){
+    hours = 0;
+    cost = 0;
 }
 
 Teacher::Teacher(string name, float salary, int hours, int cost)
+    :Employee(name, salary)
 {
+    this->hours = hours;
+    this->cost = cost;
 }
 
-void Teacher::setHours(int hours)
-{
+void Teacher::setHours(int hours){ this->hours = hours; }
+void Teacher::setCost(int cost){  this->cost = cost; }
+int Teacher::getHours() const{    return hours;}
+int Teacher::getCost() const{    return cost;}
+
+void Teacher::showInfo() const{
+    Employee::showInfo(); // name, salary
+    cout << "Hours: " << hours << endl;
+    cout << "Cost:  " << cost << " UAH" << endl;
 }
 
-void Teacher::setCost(int cost)
-{
-}
-
-int Teacher::getHours() const
-{
-    return 0;
-}
-
-int Teacher::getCost() const
-{
-    return 0;
-}
-
-void Teacher::showInfo() const
-{
-}
-
-float Teacher::calcSalary() const
-{
-    return 0.0f;
+float Teacher::calcSalary() const{
+    return (salary + hours * cost) * 0.805;
 }
